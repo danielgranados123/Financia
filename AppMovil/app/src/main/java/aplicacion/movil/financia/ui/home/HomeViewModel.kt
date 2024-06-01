a86c9610a38fe221f3fb9592c32426382998d788
Daniel Granados <20200008@ricaldone.edu.sv>
2024-06-01 09:33:53 -0600

    Creacion archivo de android

---
+++ AppMovil/app/src/main/java/aplicacion/movil/financia/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package aplicacion.movil.financia.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
