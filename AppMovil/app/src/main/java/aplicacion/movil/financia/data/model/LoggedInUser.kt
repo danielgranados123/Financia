99d3d26b67e7211f9ffc46ce58afeea3797c363e
Daniel Granados <20200008@ricaldone.edu.sv>
2024-06-01 16:05:56 -0600

    Empece a hacer el login

---
+++ AppMovil/app/src/main/java/aplicacion/movil/financia/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package aplicacion.movil.financia.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
