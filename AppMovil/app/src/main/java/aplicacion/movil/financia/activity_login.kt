05f3470c4820531b5837eece092703a306992160
Olga <20220525@ricaldone.edu.sv>
2024-06-14 22:43:43 -0600

    avance de login y de registro

--- AppMovil/app/src/main/java/aplicacion/movil/financia/activity_login.kt
+++ AppMovil/app/src/main/java/aplicacion/movil/financia/activity_login.kt
@@ -23,29 +23,18 @@ class activity_login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //1- Mandamos a traer a todos los elementos de la vista
         val txtNombredeUsuario = findViewById<EditText>(R.id.txtNombreDeUsuario)
         val  txtCorreoElectronico = findViewById<EditText>(R.id.txtCorreoElectronico)
         val btnIngresarLogin = findViewById<Button>(R.id.btnIngresarLogin)
 
-        //2- Programo los botones
         btnIngresarLogin.setOnClickListener {
-            //preparo el intent para cambiar a la pantalla de bienvenida
             val pantallaPrincipal = Intent(this, registro::class.java)
-            //Dentro de una corrutina hago un select en la base de datos
             GlobalScope.launch(Dispatchers.IO) {
-                //1-Creo un objeto de la clase conexion
                 val objConexion = Conexion().cadenaConexion()
-                //2- Creo una variable que contenga un PrepareStatement
-                //MUCHA ATENCION! hace un select where el correo y la contraseña sean iguales a
-                //los que el usuario escribe
-                //Si el select encuentra un resultado es por que el usuario y contraseña si están
-                //en la base de datos, si se equivoca al escribir algo, no encontrará nada el select
                 val comprobarUsuario = objConexion?.prepareStatement("SELECT * FROM tbUsuarios WHERE correoElectronico = ? AND clave = ?")!!
                 comprobarUsuario.setString(1, txtCorreoElectronico.text.toString())
                 comprobarUsuario.setString(2, txtNombredeUsuario.text.toString())
                 val resultado = comprobarUsuario.executeQuery()
-                //Si encuentra un resultado
                 if (resultado.next()) {
                     startActivity(pantallaPrincipal)
                 } else {
@@ -56,7 +45,7 @@ class activity_login : AppCompatActivity() {
 
         btnIngresarLogin.setOnClickListener {
             //Cambio de pantalla
-            val pantallaRegistrarme = Intent(this, registrarse::class.java)
+            val pantallaRegistrarme = Intent(this, registro::class.java)
             startActivity(pantallaRegistrarme)
         }
 
--- AppMovil/app/src/main/java/aplicacion/movil/financia/registro.kt
+++ AppMovil/app/src/main/java/aplicacion/movil/financia/registro.kt
@@ -1,10 +1,22 @@
 package aplicacion.movil.financia
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import aplicacion.movil.financia.data.model.Conexion
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
 
 class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +28,34 @@ class registro : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val txtNombreDeUsuario = findViewById<EditText>(R.id.txtNombreDeUsuario)
+        val txtContraseña = findViewById<EditText>(R.id.txtContraseña)
+        val txtCorreoElectronico = findViewById<EditText>(R.id.txtCorreoElectronico)
+        val btnIngresarLogin = findViewById<Button>(R.id.btnIngresarLogin)
+        val btnregistrarse = findViewById<Button>(R.id.btnregistrarse)
+
+
+        btnIngresarLogin.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                //Creo un objeto de la clase conexion
+                val objConexion = Conexion().cadenaConexion()
+
+                //Creo una variable que contenga un PrepareStatement
+                val crearUsuario =
+                    objConexion?.prepareStatement("INSERT INTO tbUsuarios(UUID_usuario, correoElectronico, clave) VALUES (?, ?, ?)")!!
+                crearUsuario.setString(1, UUID.randomUUID().toString())
+                crearUsuario.setString(2, txtNombreDeUsuario.text.toString())
+                crearUsuario.setString(3, txtContraseña.text.toString())
+                crearUsuario.executeUpdate()
+                withContext(Dispatchers.Main) {
+                    txtNombreDeUsuario.setText("")
+                    txtCorreoElectronico.setText("")
+                    txtContraseña.setText("")
+                }
+            }
+
+        }
+
+
     }
 }
